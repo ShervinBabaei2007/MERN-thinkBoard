93fca938a2e23dc1540ec123489bd8873b832829
Shervin Babaei <shervinb22@gmail.com>
2026-03-28 19:04:00 -0700

    refactor: move connectdb(), add .then() launch app

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,15 +10,15 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5001;
 
-connectDB();
-
 app.use(express.json());
 app.use(ratelimiter);
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
-  console.log(`Server started on PORT: ${port}`);
+connectDB().then(() => {
+  app.listen(5001, () => {
+    console.log(`Server started on PORT: ${port}`);
+  });
 });
 
 // mongodb+srv://shervinb22_db_user:j5ymLBKOATImuCEa@cluster0.choyy4y.mongodb.net/?appName=Cluster0
