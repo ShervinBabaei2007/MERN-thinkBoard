18fe06f4b38fb5235fd9602cd662719c17f36db4
Shervin Babaei <shervinb22@gmail.com>
2026-03-28 20:56:23 -0700

    feat: add cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import "dotenv/config";
 import express from "express";
+
 import { connectDB } from "./config/db.js";
 import ratelimiter from "./middleware/rateLimiter.js";
 import notesRoutes from "./routes/notesRoutes.js";
@@ -10,6 +12,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5001;
 
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use(ratelimiter);
 
